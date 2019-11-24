eaf49a0684ce523e913d9bf99a5da6e2779eb005
Conor <conorm456@hotmail.com>
2019-11-24 00:06:27 +0000

    lab 7 complete

--- lab7_word_games.py
+++ lab7_word_games.py
@@ -3,19 +3,32 @@
 # author: B. Schoen-Phelan
 # date: 07-11-2019
 # purpose: Lab 7 - Word Games
+import random
 
 class WordGames:
     def __init__(self):
-        self.mywords = input("Please enter a word or sentence: ")
+        self.myWords = input("Please enter a word or sentence: ")
         self.word_play()
 
     def word_play(self):
-        print("User input was: "+self.mywords)
+        print("User input was: " + self.myWords)
+
+
+
 
 class WordDupli(WordGames):
-    pass
+    dupe = WordGames.myWords * 2
+
+    print("Duplicated: ")
+    print(dupe)
 
 class WordScramble(WordGames):
-    pass
+    scramble = WordGames.myWords.split()
+
+    scramble = [''.join(random.sample(word, len(word))) for word in scramble]
+    print("Scrambled:")
+    print(scramble)
+
+
 
 wg = WordGames()
